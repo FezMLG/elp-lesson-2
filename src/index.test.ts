0668adf2553339e318896222d1e96e72bca3608f
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-23 11:50:55 +0100

    should throw callback after time in s with fake time

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { defer, setTimeoutInSeconds } from "./index";
+import { defer, setTimeoutInMinutes, setTimeoutInSeconds } from "./index";
 
 describe("Defer function", () => {
   it("should throw callback", () => {
@@ -7,15 +7,52 @@ describe("Defer function", () => {
 
     expect(temp).toHaveBeenCalledTimes(1);
   });
+});
 
-  test("should throw callback after time in s", (done) => {
-    // jest.useFakeTimers();
+describe("setTimeoutInSeconds", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+  // test("should throw callback after time in s", (done) => {
+  //   const temp = jest.fn();
+  //   expect.assertions(1);
+  //   function callback() {
+  //     temp();
+  //     expect(temp).toHaveBeenCalled();
+  //     done();
+  //   }
+  //   setTimeoutInSeconds(callback, 1);
+  // });
+
+  test("should throw callback after time in s with fake time", (done) => {
+    const mock = jest.fn();
+
+    expect.assertions(2);
+
+    function callback() {
+      mock();
+      expect(mock).toHaveBeenCalledTimes(1);
+      done();
+    }
+
+    setTimeoutInSeconds(callback, 1);
+    jest.advanceTimersByTime(999);
+    expect(mock).not.toBeCalled();
+    jest.advanceTimersByTime(1);
+  });
+
+  test("should throw callback after time in m", (done) => {
     const temp = jest.fn();
+    expect.assertions(1);
     function callback() {
       temp();
       expect(temp).toHaveBeenCalled();
       done();
     }
-    setTimeoutInSeconds(callback, 1);
+    setTimeoutInMinutes(callback, 1);
   });
 });
